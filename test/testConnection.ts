31854dfba3499344e1b34fc394d81e38537a3659
Noel_Cabus <noel-stage@itoa.it>
2024-04-05 19:30:41 +0200

    changes in the test script

--- test/testConnection.ts
+++ test/testConnection.ts
@@ -1,17 +1,62 @@
 #!/usr/bin/env ts-node
 
 import { Socket } from 'net';
+import readline from 'node:readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
 const host = '127.0.0.1';
 const port = 7070;
 const client = new Socket();
 
-client.connect(port, host, () => {
-    console.log("Connected to server");
+const optionsMenu = () => {
+    console.log("--- OPTIONS ---");
+    console.log("0.- ping");
+    console.log("1.- get all celestials");
+    console.log("2.- get all spaceships");
+    console.log("3.- exit");
+}
 
-    client.write('get celestials {}');
-});
+const queryBackend = async (query: string): Promise<boolean> => {
+    client.removeAllListeners();
+    return new Promise((resolve, reject) => {
+        client.connect(port, host, () => {
+            console.log("Connected to server");
 
-client.on('data', (data) => {
-    console.log(String(data));
-})
+            client.write(query);
+        });
+
+        client.on('data', (data) => {
+            console.log(String(data));
+            resolve(true);
+        });
+
+        client.on("error", (error) => {
+            console.error(error);
+            resolve(false);
+        });
+
+        client.on("close", (hadError) => {
+            console.log("closing");
+            client.end();
+        })
+    })
+}
+
+const askForInput = () => {
+    optionsMenu();
+    rl.question("", async (input) => {
+        switch (input) {
+            case '0': await queryBackend('ping'); break;
+            case '1': await queryBackend('get celestials {}'); break;
+            case '2': await queryBackend('get spaceships {}'); break;
+            case '3': return rl.close();
+        }
+        askForInput();
+    });
+}
+
+askForInput();
